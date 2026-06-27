86ba034b8fd0e9b87b00fda007733ec76856d1e2
Spirzen <tim.tagirov@mail.ru>
2026-06-28 03:27:12 +0500

    redirect animation

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import {defineConfig} from 'astro/config';
 import path from 'node:path';
 import {fileURLToPath} from 'node:url';
+import patchAstroRedirects from '../it-portals/packages/shared/src/integrations/patch-astro-redirects.mjs';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const site = process.env.IT_TOOLS_SITE ?? 'http://localhost:4334';
@@ -11,6 +12,7 @@ export default defineConfig({
   base,
   output: 'static',
   trailingSlash: 'never',
+  integrations: [patchAstroRedirects()],
   vite: {
     resolve: {
       alias: {
